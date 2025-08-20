09ee3abaa9c0a611678f33fa3f27d2ca2c64f03e
lipengpai <pengpai004@gmail.com>
2025-08-21 00:11:35 +0800

    haven't finished it

--- proj2b/src/main/HyponymsHandler.java
+++ proj2b/src/main/HyponymsHandler.java
@@ -1,64 +1,110 @@
 package main;
 import browser.NgordnetQuery;
 import browser.NgordnetQueryHandler;
+import edu.princeton.cs.algs4.In;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class HyponymsHandler extends NgordnetQueryHandler{
-    /*private class Node{
-        private int id;
-        private String word;
-        public Node(int id , String word){
-            this.id = id;
-            this.word=word;
-        }
-    }*/
-    private class Graphs{
-        private Map<Integer,ArrayList<Integer>> array;
-        //private Map<Integer,String> nodes;
-       public Graphs(int id , String word){
-              array.put(id, new ArrayList<>());
-        }
-        public ArrayList<Integer> getEdges(int v){
-              return array.get(v);
-        }
-        public void addEdge(int v ,int w){//创建从节点1到节点2的单向边
-              if(!array.containsKey(v)){
-                  array.put(v,new ArrayList<>());
-                  array.get(v).addLast(w);
-              }
-              else{
-                  array.get(v).addLast(w);
-              }
-        }
-        public ArrayList<Integer> ReachableNodes(int v){
-           ArrayList<Integer> res = new ArrayList<>();
-           res.add(v);
+public class HyponymsHandler extends NgordnetQueryHandler {
+    public class IntStringPair {
+        private Integer intKey;
+        private String strValue;
 
+        public IntStringPair(Integer intKey, String strValue) {
+            this.intKey = intKey;
+            this.strValue = strValue;
         }
-        public class DepthFirstPaths{
-           private boolean[] marked;
-           private int[] edgeTo;
-           private int s;
 
-           public DepthFirstPaths(Graphs p,int s){
+        public Integer getIntKey() {
+            return intKey;
+        }
 
-           }
-           private void dfs(Graphs p,int v){
-               marked[s] = true;
-               for(int w : p.getEdges(v)){
-                   if(!marked[w]){
-                       edgeTo[w]=v;
-                       dfs(p,w);
-                   }
-               }
-           }
+        public String getStrValue() {
+            return strValue;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            IntStringPair that = (IntStringPair) o;
+            return Objects.equals(intKey, that.intKey) && Objects.equals(strValue, that.strValue);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(intKey, strValue);
         }
     }
-    @Override
-    public String handle(NgordnetQuery q) {
-        return "Hello!";
+
+    public class Helper {
+        public HashMap<IntStringPair, ArrayList<IntStringPair>> sideMap;
+
+        public void Helper() {
+            sideMap = new HashMap<>();
+        }
+
+        public void insert(IntStringPair s1, IntStringPair s2) {
+            if (sideMap.containsKey(s1)) {
+                sideMap.get(s1).add(s2);
+            } else {
+                ArrayList<IntStringPair> newList = new ArrayList<>();
+                newList.add(s2);
+                sideMap.put(s1, newList);
+            }
+        }
+
+        public void insert(IntStringPair s1) {
+            if (sideMap.containsKey(s1)) {
+                return;
+            } else {
+                sideMap.put(s1, new ArrayList<>());
+            }
+        }
+
     }
-}
+
+    private class Ngordent {
+        public Helper my_map;
+
+        public Ngordent(String File1, String File2) {
+            my_map = new Helper();
+            In in1 = new In(File1);
+            In in2 = new In(File2);
+            while (in1.hasNextLine()) {
+                String line = in1.readLine();
+                String[] columns = line.split(",");
+                int id = Integer.parseInt(columns[0]);
+                String word = columns[1];
+                IntStringPair pair = new IntStringPair(id, word);
+                my_map.insert(pair);//这里现在有n组键值对,但对应的桶都为空
+            }
+            while (in2.hasNextLine()) {
+                String line = in2.readLine();
+                String[] columns = line.split(",");
+                int id1 = Integer.parseInt(columns[0]);
+                for (IntStringPair map : my_map.sideMap.keySet()) {
+                    if (map.getIntKey().equals(id1)) {
+                        int cnt = 1;
+                        while (cnt < columns.length) {
+                            for (IntStringPair possible_map : my_map.sideMap.keySet()) {
+                                int tempid = Integer.parseInt(columns[cnt]);
+                                if (possible_map.getIntKey().equals(tempid)) {
+                                    my_map.insert(map, possible_map);
+                                    cnt++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+        @Override
+        public String handle(NgordnetQuery q) {
+            return "Hello!";
+        }
+    }
+
+
